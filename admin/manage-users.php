e42638361d0dd4918ed6cc36e1db2112f45c3c89
shafaq-here <shafaqisrail@gmail.com>
2024-03-03 00:29:40 +0530

    Hide the aside links for non admin user

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -21,6 +21,7 @@ include 'partials/header.php'
                         <h5>Manage Posts</h5>
                     </a>
                 </li>
+                <?php if(isset($_SESSION['user_is_admin'])): ?>
                 <li>
                     <a href="<?php echo ROOT_URL ?>admin/add-user.php"><i class="uil uil-user-plus"></i>
                         <h5>Add User</h5>
@@ -41,6 +42,7 @@ include 'partials/header.php'
                         <h5>Manage Categories</h5>
                     </a>
                 </li>
+                <?php endif ?>
             </ul>
         </aside>
         <main>
