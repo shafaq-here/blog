f0f65e9ed4aabaac489f0c3e41e4112425aa1788
shafaq-here <shafaqisrail@gmail.com>
2024-03-30 03:32:20 +0530

    Display error messages and input data using value attribute

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,26 +1,38 @@
-<?php 
-    include 'partials/header.php';
+<?php
+include 'partials/header.php';
+
+//get the data that
+
+$title =  $_SESSION['add-category-data']['title'] ?? null;
+$description =  $_SESSION['add-category-data']['description'] ?? null;
+
+unset($_SESSION['add-category-data']) ;
+
 ?>
 
-    <section class="form__section">
-        <div class="container form__section-container">
-            <h2>Add Category</h2>
-            <!-- We would want to display some alert messages, error or success so we create a separate div for that -->
+<section class="form__section">
+    <div class="container form__section-container">
+        <h2>Add Category</h2>
+        <!-- We would want to display some alert messages, error or success so we create a separate div for that -->
+        <?php if (isset($_SESSION['add-category-error'])) : ?>
             <div class="alert__message error">
-                <p>This is an error message</p>
+                <?= $_SESSION['add-category-error'];
+                unset($_SESSION['add-category-error']);
+                ?>
             </div>
-            <form action="<?= ROOT_URL?>admin/add-category-logic.php" method="POST" >
-                <input type="text" name="title" placeholder="Title">
-                <textarea rows="5" name="description" placeholder="Description"></textarea>
-                
-                
-                <button type="submit" name="submit" class="btn">Add Category</button>
-    
+        <?php endif ?>
+        <form action="<?= ROOT_URL ?>admin/add-category-logic.php" method="POST">
+            <input type="text" value="<?= $title ?>" name="title" placeholder="Title">
+            <textarea rows="5" name="description" placeholder="Description"><?= $description ?></textarea>
 
-            </form>
-        </div>
-    </section>
 
-<?php 
-    include('../partials/footer.php') ;
+            <button type="submit" name="submit" class="btn">Add Category</button>
+
+
+        </form>
+    </div>
+</section>
+
+<?php
+include('../partials/footer.php');
 ?>
