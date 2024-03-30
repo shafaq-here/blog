59babc7f3373eba662cce2acccceb4d82dd268c1
shafaq-here <shafaqisrail@gmail.com>
2024-03-31 02:16:12 +0530

    Add alert messages for add and edit category

--- admin/manage-categories.php
+++ admin/manage-categories.php
@@ -7,6 +7,37 @@ $categories = mysqli_query($connection,$query) ;
 ?>
 
 <section class="dashboard">
+<?php if (isset($_SESSION['add-category-success'])) : //print the success message everytime a new category is added by the admin
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?php
+                echo $_SESSION['add-category-success'];
+                unset($_SESSION['add-category-success'])
+                ?>
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['edit-category-success'])) : //print the success message everytime a category is updated by the admin
+    ?>
+        <div class="alert__message success container">
+            <p>
+                <?php
+                echo $_SESSION['edit-category-success'];
+                unset($_SESSION['edit-category-success'])
+                ?>
+            </p>
+        </div>
+    <?php elseif (isset($_SESSION['edit-category-error'])) : //print the error message everytime a user is failed to be updated by the admin
+    ?>
+        <div class="alert__message error container">
+            <p>
+                <?php
+                echo $_SESSION['edit-category-error'];
+                unset($_SESSION['edit-category-error'])
+                ?>
+            </p>
+        </div>
+    <?php endif ?>
     <div class="container dashboard__container">
         <button id="show__sidebar-btn" class="sidebar__toggle">
             <i class="uil uil-angle-right-b"></i>
