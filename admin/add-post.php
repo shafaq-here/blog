72fbc857a3f9529d739eed68607250bd977d0f41
shafaq-here <shafaqisrail@gmail.com>
2024-03-31 19:10:49 +0530

    fetch all the categories for the select option and set post action to the logic page created

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,38 +1,50 @@
-<?php 
-include 'partials/header.php'
+<?php
+include 'partials/header.php' ;
+
+//fetch all the categories
+
+$query = "SELECT * from categories" ;
+$categories = mysqli_query($connection, $query) ;
+
+
 ?>
-    <section class="form__section">
-        <div class="container form__section-container">
-            <h2>Add Post</h2>
-            <!-- We would want to display some alert messages, error or success so we create a separate div for that -->
-            <div class="alert__message error">
-                <p>This is an error message</p>
-            </div>
-            <form action="" enctype="multipart/form-data">
-                <input type="text" placeholder="Title">
-                <select>
-                    <option value="1">Food</option>
-                    <option value="2">Travel</option><option value="3">Technology</option>
-                </select> 
-                <textarea rows="10" placeholder="Body"></textarea>
-                <div class="form__control inline">
-                    <input type="checkbox" id="is_featured" checked>
-                    <label for="is_featured">Featured</label>
-                </div>
-
-                <div class="form__control">
-                    <label for="thumbnail">Add Thumbnail</label>
-                    <input type="file" id="thumbnail">
-                </div>
-                
-                
-                <button type="submit" class="btn">Add Post</button>
-    
-
-            </form>
+<section class="form__section">
+    <div class="container form__section-container">
+        <h2>Add Post</h2>
+        <!-- We would want to display some alert messages, error or success so we create a separate div for that -->
+        <div class="alert__message error">
+            <p>This is an error message</p>
         </div>
-    </section>
+        <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
+            <input type="text" name="title" placeholder="Title">
 
-<?php 
+            <select name="category">
+                <?php while($category = mysqli_fetch_assoc($categories)) :?>
+                <option value="<?= $category['id'] ?>"> <?= $category['title'] ?></option>
+                <?php endwhile ?>
+            </select>
+            <textarea rows="10" name="body" placeholder="Body"></textarea>
+            <!-- the ability to add a featured post should only be available to admins -->
+            <?php if(isset($_SESSION['user_is_admin'])) : ?>
+            <div class="form__control inline">
+                <input type="checkbox" name="is_featured" value="1" id="is_featured" checked>
+                <label for="is_featured">Featured</label>
+            </div>
+            <?php endif ?>
+
+            <div class="form__control">
+                <label for="thumbnail">Add Thumbnail</label>
+                <input type="file" name="thumbnail" id="thumbnail">
+            </div>
+
+
+            <button type="submit" name="submit" class="btn">Add Post</button>
+
+
+        </form>
+    </div>
+</section>
+
+<?php
 include '../partials/footer.php'
 ?>
