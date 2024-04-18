5f8d7063959ce3c0e2c3cd3d6d7615ab0480d8ac
shafaq-here <shafaqisrail@gmail.com>
2024-04-18 15:24:14 +0530

    Display error messages while adding a blog post

--- admin/add-post.php
+++ admin/add-post.php
@@ -7,23 +7,35 @@ $query = "SELECT * from categories" ;
 $categories = mysqli_query($connection, $query) ;
 
 
+//get all the form data
+$title = $_SESSION['add-post-data']['title'] ?? null ;
+$body = $_SESSION['add-post-data']['body'] ?? null ;
+
+
+unset($_SESSION['add-post-data']) ;
+
+
 ?>
 <section class="form__section">
     <div class="container form__section-container">
         <h2>Add Post</h2>
         <!-- We would want to display some alert messages, error or success so we create a separate div for that -->
-        <div class="alert__message error">
-            <p>This is an error message</p>
-        </div>
+        <?php if (isset($_SESSION['add-post-error'])) : ?>
+            <div class="alert__message error">
+                <?= $_SESSION['add-post-error'];
+                unset($_SESSION['add-post-error']);
+                ?>
+            </div>
+        <?php endif ?>
         <form action="<?= ROOT_URL ?>admin/add-post-logic.php" enctype="multipart/form-data" method="POST">
-            <input type="text" name="title" placeholder="Title">
+            <input type="text" name="title" value="<?= $title ?>" placeholder="Title">
 
             <select name="category">
                 <?php while($category = mysqli_fetch_assoc($categories)) :?>
                 <option value="<?= $category['id'] ?>"> <?= $category['title'] ?></option>
                 <?php endwhile ?>
             </select>
-            <textarea rows="10" name="body" placeholder="Body"></textarea>
+            <textarea rows="10" name="body" placeholder="Body"><?= $body ?></textarea>
             <!-- the ability to add a featured post should only be available to admins -->
             <?php if(isset($_SESSION['user_is_admin'])) : ?>
             <div class="form__control inline">
